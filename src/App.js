ed7891e7d36b116c498409466b2b2e8dd5bda371
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2021-12-23 21:29:21 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import FormComponent from "./components/form/FormComponent";
+import Contacts from "./components/contacts/Contacts";
+import { useState } from "react";
+import { addInfo, updateInfo } from "./utils/function";
+
+const initialValues = { username: "", phoneNumber: "", gender: "No Info" };
+function App() {
+  const [info, setInfo] = useState(initialValues);
+
+  const handleFormSubmit = () => {
+    console.log(info);
+    if (info.id) {
+      updateInfo(info);
+    } else {
+      addInfo(info);
+    }
+  };
+  const editHandler = (id, username, phoneNumber, gender) => {
+    setInfo({ id, username, phoneNumber, gender });
+  };
+  return (
+    <div className="App">
+      <FormComponent
+        info={info}
+        setInfo={setInfo}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <Contacts editHandler={editHandler} />
+    </div>
+  );
+}
+
+export default App;
